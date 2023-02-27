ba93b4247557f1db2dbef23ef115e2c625580e11
Aaron Barratt <abarratt@userzoom.com>
2023-02-27 16:38:55 +0000

    fizzbuzz but better

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-def fizz_buzz(number):
-    for x in range(number):
-        y = x + 1
-        if y % 3 == 0 and y % 5 == 0:
-            print(f"fizz buzz")
-        elif y % 5 == 0:
-            print(f"buzz")
-        elif y % 3 == 0:
-            print(f"fizz")
-        else:
-            print(x + 1)
+def fizz_buzz(start: int = 1, end: int = 100, factors: tuple = (3, 5)):
+    factors_len = len(factors)
+    factor0 = False
+    factor1 = False
+    result = []
+
+    if factors_len != 2:
+        raise Exception(
+            f"factors length is {factors_len}. factors must be a length of 2."
+        )
+    if end <= start:
+        raise Exception(f"end must be greater than start. start.")
+    else:
+        for x in range(start, end + 1):
+            for y, factor in enumerate(factors):
+                if x % factor == 0 and y == 0:
+                    factor0 = True
+                if x % factor == 0 and y == 1:
+                    factor1 = True
+            if factor0 and factor1:
+                result.append("Fizz Buzz")
+            elif factor0:
+                result.append("Fizz")
+            elif factor1:
+                result.append("Buzz")
+            else:
+                result.append(str(x))
+            # finally
+            factor0 = False
+            factor1 = False
+
+    return result
 
 
-fizz_buzz(100)
+fizz_buzz()
