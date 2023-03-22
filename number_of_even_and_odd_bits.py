51d27c4259b7ec3b265680fbff6e39356d58e9bf
Aaron Barratt <aarontbarratt@gmail.com>
2023-03-22 14:17:45 +0000

    numer of odd and even bits solution

--- number_of_even_and_odd_bits.py
+++ number_of_even_and_odd_bits.py
@@ -1,10 +1,19 @@
-# TODO: Solve this please
+from random import randint
+
+
 class Solution:
     def evenOddBit(self, n: int) -> list[int]:
-        pass
+        answer = [0, 0]
+        for index, bit in enumerate(bin(n)[2:][::-1]):
+            if index % 2 == 0 and bit == "1":
+                answer[0] += 1
+            elif index % 2 == 1 and bit == "1":
+                answer[1] += 1
+
+        return answer
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     s = Solution()
-    print(s.evenOddBit(17))
-    print(s.evenOddBit(2))
+    for x in range(10):
+        print(s.evenOddBit(randint(1, 1000  )))
