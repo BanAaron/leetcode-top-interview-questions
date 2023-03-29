31fd1cc9608896c7bfcd709bb034a80196535798
Aaron Barratt <aarontbarratt@gmail.com>
2023-03-29 23:45:32 +0100

    removed redundant if statement in valid_parentheses.py

--- solutions/valid_parentheses.py
+++ solutions/valid_parentheses.py
@@ -4,11 +4,11 @@ class Solution:
         valid_brackets = ("()", "[]", "{}")
 
         for char in s:
-            if char in "([{":
-                stack += char
-            elif char in ")]}":
-                stack += char
+            stack += char
+            if char in ")]}":
+                # if the second to last character + last character valid breaks
                 if stack[len(stack) - 2:] in valid_brackets:
+                    # remove final two characters
                     stack = stack[:-2]
 
         return len(stack) == 0
