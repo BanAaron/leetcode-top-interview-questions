d17dcf2cea307358a7e40e2267fa12adf6a28698
Aaron Barratt <aarontbarratt@gmail.com>
2023-03-30 17:18:53 +0100

    Add Binary solution

--- solutions/add_binary.py
+++ solutions/add_binary.py
@@ -2,7 +2,7 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         # convert the strings to base 2 integer, add them together, then convert back to binary
         # [2:] removed the first 2 characters "0b" which is not needed
-        return bin(int(a, 2) + int(b, 2))[:]
+        return bin(int(a, 2) + int(b, 2))[2:]
 
 
 if __name__ == "__main__":
