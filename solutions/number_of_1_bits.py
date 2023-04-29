5cb1be2d72fba6448e0b535eab688d75bcc253f7
Aaron <aarontbarratt@gmail.com>
2023-04-30 00:11:52 +0100

    💻 number_of_1_bits.py solution

---
+++ solutions/number_of_1_bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        # convert to binary then use count() to find the sum of 1s
+        return bin(n).count("1")
+
+        # initial solution
+        # return sum([int(x) for x in bin(n) if x == "1"])
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.hammingWeight(11))
