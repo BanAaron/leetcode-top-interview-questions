27b782c72367b20b030c5b9c7eb36234a7499aaf
Aaron Barratt <aarontbarratt@gmail.com>
2023-09-25 22:57:23 +0100

    sort colors solution

---
+++ solutions/sort_colors.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        while True:
+            swapped = False
+            for i, _ in enumerate(nums):
+                if i < len(nums) - 1 and nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                    swapped = True
+            if not swapped:
+                break
+        print(nums)
+
+
+if __name__ == "__main__":
+    c = Solution()
+    nums = [1, 2, 0]
+    c.sortColors(nums)
+    nums = [2, 0, 2, 1, 1, 0]
+    c.sortColors(nums)
+    nums = [2, 0, 1]
+    c.sortColors(nums)
