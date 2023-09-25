9c87b9e0cf87364aff12c39ceef59c88ff463388
Aaron Barratt <aarontbarratt@gmail.com>
2023-09-25 23:07:47 +0100

    sort colors solution

--- solutions/sort_colors.py
+++ solutions/sort_colors.py
@@ -15,10 +15,10 @@ class Solution:
 
 
 if __name__ == "__main__":
-    c = Solution()
+    s = Solution()
     nums = [1, 2, 0]
-    c.sortColors(nums)
+    s.sortColors(nums)
     nums = [2, 0, 2, 1, 1, 0]
-    c.sortColors(nums)
+    s.sortColors(nums)
     nums = [2, 0, 1]
-    c.sortColors(nums)
+    s.sortColors(nums)
